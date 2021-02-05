6609a95cb9c4aa63ffbf97d2ee255545dfb3abc7
Matthew Clark <mclark@drmatthewclark.com>
2021-02-04 21:56:32 -0500

    updates to improve robustnes

--- bluetooth.py
+++ bluetooth.py
@@ -10,64 +10,58 @@ topic = 'home'
 user = 'alarm'
 host_name = 'alarm'
 process = None
+client  = None
 
 # count number of people who are home
 statusquery = 'select count(person)  from (select distinct on (person,location) time, person, location, home from bluetooth order by person, location, time desc) a where home= true;'
-insert_sql = "insert into bluetooth(time, location, person, home) values (to_timestamp(%s), %s, %s, %s );"
-query_sql  = "select home from bluetooth where person = %s order by time desc limit 1"
 
-currentCount = None  # number of people home
+insert_sql = "insert into bluetooth(time, location, person, home) values (to_timestamp(%s), %s, %s, %s );"
+query_sql  = "select home from bluetooth where person = %s and location = %s order by time desc limit 1"
+
 
 def on_connect(client, obj, flags, rc):
     client.subscribe(topic)
-    print('subscribing',client, obj, flags,rc)
 
+
+def decode_home(home):
+    homeb = False
+    if home == '1' or home == 1 or home == True:
+        homeb = True
+
+    return homeb
 
 def on_message(client, userdata, message):
+
     msg =  str(message.payload.decode("utf-8")).split(",")
-    print('recieved msg ', msg)
+    #print('recieved msg ', msg)
     time   = msg[0]
     locale = msg[1]
     person = msg[2]
-    home   = msg[3]
+    home   = decode_home(msg[3])
+
     insert_record( (time, locale, person, home) )
 
 
 def insert_record( values ):
-    global currentCount
     time, locale, person, home = values
 
-    homeb =  False
-    if home == '1' or home == 1 or home == True:
-        homeb = True
-
-    print(home, homeb)
-    print('insert_record values ', values)
     con = None
     try:
       con = psycopg2.connect('user=' + user )
 
       with con.cursor() as cur:
-         cur.execute(query_sql, (person,) )
+         cur.execute(query_sql, (person,locale,) )
          if cur.rowcount > 0:
-            status = cur.fetchone()[0]
+            status = bool(cur.fetchone()[0])
          else:
              status = 'init'
-             homeb = 'False'
+             home = 'False'
 
-         if status != homeb:
-            print('db ', cur.mogrify(insert_sql, values ))
+         #print('person', person, 'status', status, 'home', home)
+         if status != home:
+            print('update db - ', cur.mogrify(insert_sql, values ))
             cur.execute(insert_sql, values)
             con.commit()
-
-      with con.cursor() as cur:
-        cur.execute(statusquery)
-        count = cur.fetchone()[0]
-        if currentCount == None:
-            currentCount = count
-
-        currentCount = count
-        print('current count', count)
 
     except:
        print("insert_record error", sys.exc_info()[0])
@@ -76,8 +70,8 @@ def insert_record( values ):
 
 
 def main():
+    global client
     client = mqtt.Client(client_id="bluetooth")
-    print('loop connected to ', host_name)
     client.on_message = on_message
     client.on_connect = on_connect
     client.connect(host_name)
@@ -92,5 +86,5 @@ def start():
 
 def stop():
     print("stopping bluetooth process")
-    process.terminate()
+    client.stop()
 
